9a7ca13d2f77142fc67737bcc4c1a058c56442dc
Saumya Jha <jhasaumya554@gmail.com>
2025-10-05 20:23:58 +0530

    added loading screen

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,14 @@
 
 const msgInput = document.getElementById("messageInput");
 
+const loading = document.createElement("div");
+loading.innerHTML = `<div class="flex justify-start">
+          <div class="max-w-[80%] rounded-lg px-4 py-3  text-gray-100">
+            <p class="text-[16px] animate-pulse">Thinking...</p>
+          </div>
+        </div>`;
+
+
 async function callServer(userMsg){
     const response = await fetch('http://localhost:3000/chat' , {
       method : "POST" , 
@@ -20,7 +28,7 @@ async function callServer(userMsg){
     return result.message;
 }
 
-function addToUI(text,role){  
+function addToUI(text,role,isThinking){  
   let msgContainer = document.getElementById("messageContainer");
   let newDiv = document.createElement("div");
   if (text == "") return;
@@ -32,6 +40,11 @@ function addToUI(text,role){
           </div>
         </div>`;
   msgContainer.appendChild(newDiv);
+  if(isThinking) {
+    msgContainer.appendChild(loading);
+  } else {
+    msgContainer.removeChild(loading);
+  }
   msgInput.value = "";
 }
 
@@ -39,16 +52,19 @@ function addToUI(text,role){
 msgInput.addEventListener("keyup",async (event) => {
     if(event.key === "Enter"){
       let input = msgInput.value;
-      addToUI(input, "user");
+      if(!input) return ;
+      addToUI(input, "user",true);
       const llm_res = await callServer(input);
-      addToUI(llm_res,"assistant");
+      addToUI(llm_res,"assistant",false);
     }
   })
   
-  const sendBtn = document.getElementById("sendButton")
+  const sendBtn = docum
+  ent.getElementById("sendButton")
   sendBtn.addEventListener("click" , async () => {
-  let input = msgInput.value;
-  addToUI(input,"user");
+    let input = msgInput.value;
+    if(!input) return ;
+  addToUI(input,"user",true);
   const llm_res = await callServer(input);
-  addToUI(llm_res,"demo");
+  addToUI(llm_res,"demo",false);
 })
