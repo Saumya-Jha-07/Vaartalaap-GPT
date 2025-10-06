eca1990d76a55612e3ca1beae794c63f4d19ccf0
Saumya Jha <jhasaumya554@gmail.com>
2025-10-06 21:44:59 +0530

    bs changed thinking to sabar kar

--- frontend/script.js
+++ frontend/script.js
@@ -1,39 +1,40 @@
-const threadId = Date.now().toString(36) + Math.random.toString(36)
+const threadId = Date.now().toString(36) + Math.random.toString(36);
 
 const msgInput = document.getElementById("messageInput");
 
 const loading = document.createElement("div");
 loading.innerHTML = `<div class="flex justify-start">
           <div class="max-w-[80%] rounded-lg px-4 py-3  text-gray-100">
-            <p class="text-[16px] animate-pulse">Thinking...</p>
+            <p class="text-[16px] animate-pulse">Sabar kr bhutni ke...</p>
           </div>
         </div>`;
 
+async function callServer(userMsg) {
+  const response = await fetch("http://localhost:3000/chat", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ message: userMsg, threadId: threadId }),
+  });
 
-async function callServer(userMsg){
-    const response = await fetch('http://localhost:3000/chat' , {
-      method : "POST" , 
-      headers : {
-        'content-type' : 'application/json'
-      },
-      body : JSON.stringify({message : userMsg , threadId : threadId})
-    })
+  if (!response.ok) {
+    throw new Error("Error generating the response!");
+  }
 
-    if(!response.ok){
-      throw new Error("Error generating the response!")
-    }
+  const result = await response.json();
+  console.log(`user : ${userMsg} \n bot : ${result.message}`);
 
-    const result = await response.json();
-    console.log(`user : ${userMsg} \n bot : ${result.message}`)
-    
-    return result.message;
+  return result.message;
 }
 
-function addToUI(text,role,isThinking){  
+function addToUI(text, role, isThinking) {
   let msgContainer = document.getElementById("messageContainer");
   let newDiv = document.createElement("div");
   if (text == "") return;
-  newDiv.innerHTML = `<div class="flex ${role == "user" ? "justify-end" : "justify-start"}">
+  newDiv.innerHTML = `<div class="flex ${
+    role == "user" ? "justify-end" : "justify-start"
+  }">
           <div class="max-w-[80%] rounded-lg px-4 py-3 ${
             role == "user" ? `bg-[#444654]` : ""
           } text-gray-100">
@@ -41,7 +42,7 @@ function addToUI(text,role,isThinking){
           </div>
         </div>`;
   msgContainer.appendChild(newDiv);
-  if(isThinking) {
+  if (isThinking) {
     msgContainer.appendChild(loading);
   } else {
     msgContainer.removeChild(loading);
@@ -49,22 +50,21 @@ function addToUI(text,role,isThinking){
   msgInput.value = "";
 }
 
-
-msgInput.addEventListener("keyup",async (event) => {
-    if(event.key === "Enter"){
-      let input = msgInput.value;
-      if(!input) return ;
-      addToUI(input, "user",true);
-      const llm_res = await callServer(input);
-      addToUI(llm_res,"assistant",false);
-    }
-  })
-  
-  const sendBtn = document.getElementById("sendButton")
-  sendBtn.addEventListener("click" , async () => {
+msgInput.addEventListener("keyup", async (event) => {
+  if (event.key === "Enter") {
     let input = msgInput.value;
-    if(!input) return ;
-  addToUI(input,"user",true);
+    if (!input) return;
+    addToUI(input, "user", true);
+    const llm_res = await callServer(input);
+    addToUI(llm_res, "assistant", false);
+  }
+});
+
+const sendBtn = document.getElementById("sendButton");
+sendBtn.addEventListener("click", async () => {
+  let input = msgInput.value;
+  if (!input) return;
+  addToUI(input, "user", true);
   const llm_res = await callServer(input);
-  addToUI(llm_res,"demo",false);
-})
+  addToUI(llm_res, "demo", false);
+});
